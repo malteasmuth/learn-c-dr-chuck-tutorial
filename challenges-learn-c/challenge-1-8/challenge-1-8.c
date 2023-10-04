280d5c8d8d0e73cf93613d37e4bfe1c4d2ae2e39
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-10-04 14:58:14 +0200

    Solution to 1-8

--- challenges-learn-c/challenge-1-8/challenge-1-8.c
+++ challenges-learn-c/challenge-1-8/challenge-1-8.c
@@ -1,4 +1,5 @@
-/* Exercise 1-8.
+/* 04.10.23
+ * Exercise 1-8. 
  *  Write a program to replace each tab by the three-character sequence >, backspace, -, which prints as >,
  *   and each backspace by the similar sequence <. This makes tabs and backspaces visible. */
 
