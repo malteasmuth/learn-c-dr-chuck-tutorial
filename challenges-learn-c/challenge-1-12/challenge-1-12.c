73cccd89427e0241aebc09702e92ed33365fab07
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-10-12 12:10:09 +0200

    Refactor challenge

--- challenges-learn-c/challenge-1-12/challenge-1-12.c
+++ challenges-learn-c/challenge-1-12/challenge-1-12.c
@@ -1,40 +1,71 @@
-/* Exercise 1-12. Write a program to print a histogram of the lengths of words in its input.
+/* Exercise 1-12. Write a  program to print a histogram of the lengths of words in its input.
  *  It is easiest to draw the histogram horizontally; a vertical orientation is more challenging.*/
 
 #include <stdio.h>
 
 int main(){
 
-	/*Solution: We read the input. 
+	/*Solution 1: We read the input. 
 	 * While input is not EOF, if input is not \n, \t or "",
 	 * We increment wordLength by 1.
 	 * If input is \n, \t or "", we print "-" with a for loop  and set counter to 0.
 	 * Then start with the next word until we hit EOF */
 
-	/* declaration */
-	int input, wordLength, i;
+	/*Solution 2: We read the input, char by char.
+	 * 
+	 * Computation.
+	 * If input is not \n, \t or ' ', we increment wordLength by 1.
+	 * If input is \n, \t or ' ', we store wordLength at array position i and and switch to position i++
+	 *
+	 * Output.
+	 * We iterate over the array. With a for loop, we print '*' as many times as the wordLength stored in the array position.
+	 * */
 
-	wordLength = i = 0;	
+	/* declaration */
+	int input, wordLength, i, j;
+	int histogram[30];
+	int wordCounter = 0;
+
+	/* initialization*/
+	wordLength = i = j = 0;	
+	for(i = 0; i < 30; i++){
+		histogram[i] = 0;
+	}
 
 	/* get input */    
 
 	while((input = getchar()) != EOF){
 	
 		if(input == '\n' || input == '\t' || input == ' '){
-		
-			for(i = 0; i < wordLength; i++){
-			
-				printf("-");
-			}
-			
-			printf("\n");
+
+			histogram[j] = wordLength;
 			wordLength = 0;
+			wordCounter++;
+			j++;
+			
 		}
 
 		else{
-		
 			wordLength++;
 		}	
+	}
+
+	/* create output*/
+
+	printf("Length of words\n");
+	printf("---------------\n");
+
+	for(i = 0; i <= wordCounter; i++){
+	
+		printf("%d\t| ", i + 1);
+
+		for(j = 0; j < histogram[i]; j++){
+		
+			printf("*");
+		}
+
+		printf("\n");
+
 	}
 
 }
